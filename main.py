7bfddbc5518091c030f78ce77f5c35c23837b5f9
Sina Masoud-Ansari <sina.masoud.ansari@gmail.com>
2015-12-19 13:43:41 +1300

    cool effects

--- main.py
+++ main.py
@@ -34,11 +34,11 @@ def main():
     main_network_shape = (5, 5, 5)
     main_network_position = (0, 0, 0)
     main_network_spacing = (0.1, 0.1, 0.1)
-    main_network = Network(Network.STDP_NEURON, main_network_shape, main_network_position, main_network_spacing, init_random_values=False)
+    main_network = Network(Network.STDP_NEURON, main_network_shape, main_network_position, main_network_spacing, init_random_values=True)
     main_network.connect(main_network, density=0.02)
 
     # init input sensor network
-    sensor_network_shape = (1, 5, 5)
+    sensor_network_shape = (1, 1, 1)
     sensor_network_offset_x = -0.1
     #sensor_network_offset_y = -main_network_shape[1] * main_network_spacing[1]/2.0
     #sensor_network_offset_z = -main_network_shape[2] * main_network_spacing[2]/2.0
@@ -50,7 +50,7 @@ def main():
     sensor_network.connect(main_network, density=0.02)
     
     # init output neuron network
-    output_network_shape = (1, 5, 5)
+    output_network_shape = (1, 1, 1)
     output_network_offset_x = (main_network_shape[0] - 1) * main_network_spacing[0] + 0.1
     #output_network_offset_y = -main_network_shape[1] * main_network_spacing[1]/2.0
     #output_network_offset_z = -main_network_shape[2] * main_network_spacing[2]/2.0
@@ -83,7 +83,7 @@ def main():
     output_network_y = [n.position[1] for n in output_network_flat]
     output_network_z = [n.position[2] for n in output_network_flat]
 
-    """
+
     # show random main_network connections
     flat = main_network.neurons.flatten()
     index = np.random.randint(len(flat))
@@ -111,11 +111,11 @@ def main():
     for u, w in n.upstream.iteritems():
         ux, uy, uz = u.position
         network_view.plot([nx, ux], [ny, uy], [nz, uz], color='blue')
-    """
 
-    main_network_points = network_view.scatter(main_network_x, main_network_y, main_network_z, s=scatter_marker_size, depthshade=False, cmap=cm.hot, vmin=0, vmax=1.0, marker='o', c=main_network.get_values().flatten())
-    sensor_network_points = network_view.scatter(sensor_network_x, sensor_network_y, sensor_network_z, s=scatter_marker_size, depthshade=False, cmap=cm.hot, vmin=0, vmax=1.0, marker='^', c=sensor_network.get_values().flatten())
-    output_network_points = network_view.scatter(output_network_x, output_network_y, output_network_z, s=scatter_marker_size, depthshade=False, cmap=cm.hot, vmin=0, vmax=1.0, marker='s', c=output_network.get_values().flatten())
+
+    main_network_points = network_view.scatter(main_network_x, main_network_y, main_network_z, s=scatter_marker_size, depthshade=True, cmap=cm.hot, vmin=0, vmax=1.0, marker='o', c=main_network.get_values().flatten())
+    sensor_network_points = network_view.scatter(sensor_network_x, sensor_network_y, sensor_network_z, s=scatter_marker_size, depthshade=True, cmap=cm.hot, vmin=0, vmax=1.0, marker='^', c=sensor_network.get_values().flatten())
+    output_network_points = network_view.scatter(output_network_x, output_network_y, output_network_z, s=scatter_marker_size, depthshade=True, cmap=cm.hot, vmin=0, vmax=1.0, marker='s', c=output_network.get_values().flatten())
 
     network_view.set_xlabel('X')
     network_view.set_ylabel('Y')
